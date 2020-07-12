361c31dcb6cfbb12d54ff0b98320ccedf5a99b18
sups-k <49182287+sups-k@users.noreply.github.com>
2020-07-12 20:19:53 +0530

    Corrected the answer

--- Q3-pop-pred.R
+++ Q3-pop-pred.R
@@ -5,11 +5,11 @@
 library(deSolve)
 
 ## Part 1
-x <- seq(0, 50, 1)
+x <- seq(0, 100, 1)
 f1 <- (1.3 * x * (1-(x/20)) ) - (x/(1+x)) # alpha = 1.3
 f2 <- (0.3 * x * (1-(x/20)) ) - (x/(1+x)) # alpha = 0.3
 f3 <- (0.1 * x * (1-(x/20)) ) - (x/(1+x)) # alpha = 0.1
-plot(x, f1, type = "l", xlab = "x", ylab = "f(x)", ylim = c(-100, 6), col = "blue")
+plot(x, f1, type = "l", xlab = "x", ylab = "f(x)", ylim = c(-100, 100), col = "blue")
 lines(x, f2, col="red", ylim = c(-100, 6))
 lines(x, f3, col="green", ylim = c(-100,6))
 
@@ -19,17 +19,23 @@ f = f1 # contains all values of f1
 f <- f[vals] # selects only intersection of f1, f2, f3
 x_f <- x[vals] # selects only x values of intersection of f1, f2, f3
 
-# Mark the fixed points on the graph
+#################### ALL WRONG BELOW ##### code is right ###############
+# # Mark the fixed points on the graph
+# 
+# # First Point
+# abline(v = x_f[1], col = "cyan") # v for vertical line
+# abline(h = f[1], col = "cyan") # h for horizontal line
+# text(7,-10, paste("(", x_f[1], ", ", f[1], ")"), col = "cyan")
+# 
+# # Second Point
+# abline(v = x_f[2], col = "pink") # v for vertical line
+# abline(h = f[2], col = "pink") # h for horizontal line
+# text(30,-10, paste("(", x_f[2], ", ", f[2], ")"), col = "pink")
+###########################################################
 
-# First Point
-abline(v = x_f[1], col = "cyan") # v for vertical line
-abline(h = f[1], col = "cyan") # h for horizontal line
-text(7,-10, paste("(", x_f[1], ", ", f[1], ")"), col = "cyan")
-
-# Second Point
-abline(v = x_f[2], col = "pink") # v for vertical line
-abline(h = f[2], col = "pink") # h for horizontal line
-text(30,-10, paste("(", x_f[2], ", ", f[2], ")"), col = "pink")
+# From the graph, we can see no fixed points because none of the functions
+# reach steady state. They all intersect at carrying capacity. To reach steady
+# state, the graph must be parallel to x-axis.
 
 ## Part 2
 # Define model function with tau, initial values, parameter
@@ -61,10 +67,7 @@ plot(out, type = "l", xlab = "tau", ylab = "x(tau)")
 # Yes, there is a difference in the steady state values.
 # For x(0) = 1.2, the steady state value is 0
 # For x(0) = 3.5, the steady state value is 16
-# This is because the solution of the logistic equation part of the function is a
-# sigmoid function. The other part's solution is -x + ln|x+1| + c (haven't determined c here)
-# When the initial value changes, i.e., x(0) changes, it changes the entire x(tau) function.
-# Hence the graphs will be different.
-
-#Print summary
-print(out)
+# A steady state value of 0 means with the initial population 1.2 and alpha = 0.3,
+# the population cannot be sustained and dies out.
+# With initial population 3.5, alpha can sustain it and the population
+# grows and lives.
